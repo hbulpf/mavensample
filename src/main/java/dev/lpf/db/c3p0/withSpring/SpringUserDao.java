c3d813fd10c1756446d39ba6b157e4601edb893b
RunAtWorld <hudalpf@163.com>
2020-01-10 03:23:54 +0000

    Update SpringUserDao.java

--- src/main/java/dev/lpf/db/c3p0/withSpring/SpringUserDao.java
+++ src/main/java/dev/lpf/db/c3p0/withSpring/SpringUserDao.java
@@ -3,6 +3,8 @@ package dev.lpf.db.c3p0.withSpring;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import dev.lpf.db.c3p0.beans.User;
@@ -11,6 +13,8 @@ import dev.lpf.db.c3p0.beans.User;
  * Spring UserDao
  */
 public class SpringUserDao extends JdbcDaoSupport {
+    private static final Logger LOGGER = LoggerFactory.getLogger("dev.lpf.db.c3p0.demo.C3p0Utils");
+
     /**
      * Gets all.
      *
@@ -30,5 +34,29 @@ public class SpringUserDao extends JdbcDaoSupport {
         String sql = "select* from t_user where user_id=?";
         User user = (User) this.getJdbcTemplate().queryForObject(sql, new Object[] {id}, new UserMapper());
         return user;
+    }
+
+    /**
+     * 增加金额.
+     *
+     * @param id the id
+     * @param money the money
+     * @return the boolean
+     */
+    public boolean increase(int id, float money) {
+        String sql = "UPDATE t_user SET account=account+? WHERE user_id=?";
+        return this.getJdbcTemplate().update(sql,new Object[] {money,id}) > 0 ? true : false;
+    }
+
+    /**
+     * 减少金额
+     *
+     * @param id the id
+     * @param money the money
+     * @return the boolean
+     */
+    public boolean decrease(int id, float money) {
+        String sql = "UPDATE t_user SET account=account-? WHERE user_id=?";
+        return this.getJdbcTemplate().update(sql,new Object[] {money,id}) > 0 ? true : false;
     }
 }
