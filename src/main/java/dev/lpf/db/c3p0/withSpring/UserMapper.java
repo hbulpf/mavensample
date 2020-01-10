8bc2f68f2a813c29be02bfa30e92ef897f39a610
RunAtWorld <hudalpf@163.com>
2020-01-10 03:24:49 +0000

    Update UserMapper.java

--- src/main/java/dev/lpf/db/c3p0/withSpring/UserMapper.java
+++ src/main/java/dev/lpf/db/c3p0/withSpring/UserMapper.java
@@ -1,4 +1,3 @@
-
 package dev.lpf.db.c3p0.withSpring;
 
 /**
@@ -18,6 +17,30 @@ public class SpringUserService {
     }
 
     public void accountTransfer(float money) {
+package dev.lpf.db.c3p0.withSpring;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import dev.lpf.db.c3p0.beans.User;
+
+/**
+ * User Mapper
+ *
+ */
+public class UserMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet resultSet, int index) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("user_id"));
+        user.setUsername(resultSet.getString("user_name"));
+        user.setUserpwd(resultSet.getString("password"));
+        return user;
+    }
+}
+
         // 小明增加1000
         userDao.increase(1, money);
 
