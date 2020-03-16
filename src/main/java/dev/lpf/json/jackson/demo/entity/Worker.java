47c6637d321649f95899f1742f71c7ead7f058a8
RunAtWorld <hudalpf@163.com>
2020-03-16 16:00:53 +0800

    add src/main/java/dev/lpf/json/jackson/demo/entity/Worker.java.

---
+++ src/main/java/dev/lpf/json/jackson/demo/entity/Worker.java
@@ -0,0 +1,28 @@
+package dev.lpf.json.jackson.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Worker
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonRootName("Worker")
+public class Worker {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("age")
+    private int age;
+
+    @JsonProperty("interests")
+    private List<String> interests;
+}
