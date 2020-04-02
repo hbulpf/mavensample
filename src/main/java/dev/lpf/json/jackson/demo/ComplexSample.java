9de6a9b5b7894e17dcd0db45e373550a2ef37979
RunAtWorld <hudalpf@163.com>
2020-04-02 09:25:19 +0800

    Update ComplexSample.java

--- src/main/java/dev/lpf/json/jackson/demo/ComplexSample.java
+++ src/main/java/dev/lpf/json/jackson/demo/ComplexSample.java
@@ -25,8 +25,9 @@ public class ComplexSample {
      */
     public static void main(String[] args) {
         try {
-//            complexJavaObject();
+            //            complexJavaObject();
             readFromJsonList();
+            readFromRspJson();
         } catch (IOException e) {
             System.out.println("JSON Serializable Error: " + e.getMessage());
         }
@@ -66,17 +67,23 @@ public class ComplexSample {
         JsonNode root = mapper.readTree(str);
         String strName = root.get("name").asText();
         String strInterests = root.get("interests").asText();
+        ArrayNode hobbiesNode = (ArrayNode)root.get("interests");
+        System.out.printf("=>hobbies:\t");
+        for(JsonNode node : hobbiesNode){
+            System.out.printf("%s\t",node);
+        }
+        System.out.println();
+
         String strAge = root.get("age").asText();
         String strFriends = root.get("friends").asText();
-        System.out.println("strName:"+strName);
-        System.out.println("strInterests:"+strInterests);
-        System.out.println("strAge:"+strAge);
-        System.out.println("strFriends:"+strFriends);
-
-        System.out.println(root.get("name"));
-        System.out.println(root.get("interests"));
-        System.out.println(root.get("age"));
-        System.out.println(root.get("friends"));
+        System.out.println("\tstrName:"+strName);
+        System.out.println("\tstrInterests:"+strInterests);
+        System.out.println("\tstrAge:"+strAge);
+        System.out.println("\tstrFriends:"+strFriends);
+        System.out.println("\tname:"+root.get("name"));
+        System.out.println("\tinterests"+root.get("interests"));
+        System.out.println("\tage"+root.get("age"));
+        System.out.println("\tfriends"+root.get("friends"));
         System.out.println("worker list:");
         List<Worker> workerList = new ArrayList<>();
         if(root.get("friends").isArray()){
@@ -85,5 +92,18 @@ public class ComplexSample {
             }
         }
         System.out.println(workerList.toString());
+    }
+
+    /**
+     * 从Json返回体中读取内容
+     *
+     */
+    static void readFromRspJson() throws JsonProcessingException {
+        String str = "{\"code\":\"0\",\"data\":{\"type\":1}}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(str);
+        int code = root.get("code").asInt();
+        int type = root.get("data").get("type").asInt();
+        System.out.println(String.format("code:%d,type:%d",code,type));
     }
 }
