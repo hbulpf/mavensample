8fbca2e6f88ae3b2872b899e55f33a7ebec8ba37
RunAtWorld <hudalpf@163.com>
2020-08-03 16:12:55 +0800

    add src/main/java/dev/utils/demos/PersonCopy.java.

---
+++ src/main/java/dev/utils/demos/PersonCopy.java
@@ -0,0 +1,24 @@
+package dev.utils.demos;
+
+/**
+ * PersonCopy
+ */
+public class PersonCopy {
+    private String id;
+
+    private String name;
+
+    private int age;
+
+    private String dept;
+
+    public PersonCopy(String id, String name, int age, String dept) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+    }
+
+    public PersonCopy() {
+    }
+}
